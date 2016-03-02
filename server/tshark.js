4e12326944782c5db48785e6c56865c7ef129938
stingwatch-lubuntu-15.10x64 <stingwatch@stingwatch-lubuntu-15.10x64>
2016-03-02 04:15:17 -0600

    move tshark

--- server/tshark.js
+++ server/tshark.js
@@ -33,3 +33,46 @@ runTshark = function() {
 
   }, function () { console.log('onexit: failed to bind'); }));
 }
+
+var tsharkParser = function(csv) {
+  var config = {
+    delimiter: "",
+    newline: "",
+    dynamicTyping: true,
+  }
+
+  var readings = Papa.parse(data.toString(), config).data;
+
+  if(readings) {
+    readings.forEach(function(reading) {
+      if(reading.length > 0) {
+        reading = sanitizeReading(reading)
+        var defaultVal = -999999
+        var cid = reading[0] || defaultVal
+
+        if(cid > 0) {
+          var now = new Date()
+          Catcher.TelephonyReadings.insert({
+            cid:                      cid,
+            mnc:                      reading[1] || defaultVal,
+            mcc:                      reading[2] || defaultVal,
+            lac:                      reading[3] || defaultVal,
+            signalStrengthDBM:        reading[4] || defaultVal,
+          });
+        }
+      }
+    })
+  }
+}
+
+var sanitizeReading = function(val) {
+  var pat = /^0x.*/
+  var hexRegex = new RegExp(pat)
+
+  return reading = _.map(reading, function(v) {
+    if(hexRegex.test(v)) {
+      return parseInt(v)
+    }
+    return parseFloat(v)
+  })
+}
